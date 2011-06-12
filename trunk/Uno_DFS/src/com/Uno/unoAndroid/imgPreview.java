4a58c95cbff659cd6dfbf7e8755e1c1c43f64da2
jilong.liao@gmail.com <jilong.liao@gmail.com@32d8bba7-3cf6-fe5a-acaa-a6ced68bc69f>
2011-06-12 03:21:55 +0000

    re-organize.

--- trunk/Uno_DFS/src/com/Uno/unoAndroid/imgPreview.java
+++
@@ -1,7 +0,0 @@
-package com.Uno.unoAndroid;
-
-import android.app.Activity;
-
-public class imgPreview extends Activity {
-
-}
--- trunk/Uno_DFS/src/com/Uno/unoAndroid/txtPreview.java
+++
@@ -1,7 +0,0 @@
-package com.Uno.unoAndroid;
-
-import android.app.Activity;
-
-public class txtPreview extends Activity {
-
-}
