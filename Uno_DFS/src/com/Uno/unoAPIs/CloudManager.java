bc320403f0ba68ee39d6a44c5f8bc89bd7a461d7
jilong.liao <jilong.liao@gmail.com>
2011-08-16 21:44:56 +0000

    local part done, but not tested yet.

--- Uno_DFS/src/com/Uno/unoAPIs/CloudManager.java
+++ Uno_DFS/src/com/Uno/unoAPIs/CloudManager.java
@@ -9,11 +9,12 @@ public class CloudManager {
 	
 	/*
 	 * Client System Initialization
+	 * Attention: this step does not run Service, it needs manually run.
 	 * */
 	public void init() {}
 	
 	/*
-	 * Authentication 
+	 * Authentication
 	 * */
 	public void login(String usr, String pwd) {}
 	
@@ -23,7 +24,7 @@ public class CloudManager {
 	public void register(String usr, String pwd) {}
 	
 	/*
-	 * Push methods
+	 * Push methods: only send metadata.
 	 * */
 	public void push() {}
 	
@@ -32,23 +33,26 @@ public class CloudManager {
 	public void push(LocalSensor ls) {}
 	
 	/*
-	 * Pull methods
+	 * Pull methods: get metadata, remote sensor, remote data instance.
 	 * */
 	public void pull() {}
 	
+	// pull physical remote data to local.
 	public LocalData pull(RemoteData rd) {return null;}
 	
+	// pull remote metadata from a pariticular path.
 	public RemoteData pull(String path) {return null;}
 	
+	// pull a remote sensor from a specific path with certain type.
 	public RemoteSensor pull(String path, int type) {return null;}
 	
 	/*
-	 * Search methods
+	 * Search methods: return a group of path which contain the resource keyword.
 	 * */
 	public String [] search(String keyword) {return null;}
 	
 	/*
-	 * Backup methods
+	 * Backup methods: it will send not only the metadata, but also the physical data itself.
 	 * */
 	public void backup(LocalData ld) {}
 	
