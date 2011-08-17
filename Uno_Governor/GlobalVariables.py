5ce64c6cf795bb3113e182f90d264fbf70f362e4
jilong.liao <jilong.liao@gmail.com>
2011-08-17 18:30:58 +0000

    RemoteData done, not tested yet.

--- Uno_Governor/GlobalVariables.py
+++ Uno_Governor/GlobalVariables.py
@@ -2,4 +2,4 @@ TCP_HOST = ("little-eyes", 11314);
 UDP_HOST = ("little-eyes", 11315);
 FTP_HOST = ("little-eyes", 11316);
 
-CommandSet = ["LOGIN", "SETPUBLIC", "SETPRIVATE", "OFFLINE", "REGISTER", "GET"];
+CommandSet = ["API", "LOGIN", "SETPUBLIC", "SETPRIVATE", "OFFLINE", "REGISTER", "GET"];
