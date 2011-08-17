7d61a92b9be4e4c8bb1d22ba0754a69b09c29653
jilong.liao@gmail.com <jilong.liao@gmail.com@32d8bba7-3cf6-fe5a-acaa-a6ced68bc69f>
2011-08-17 18:31:18 +0000

    RemoteData done, not tested yet.

--- trunk/Uno_DFS/src/com/Uno/unoAPIs/UnoConstant.java
+++ trunk/Uno_DFS/src/com/Uno/unoAPIs/UnoConstant.java
@@ -3,4 +3,6 @@ package com.Uno.unoAPIs;
 public class UnoConstant {
 
 	static public String GOVERNOR_ADDRESS = "com1379.eecs.utk.edu";
+	public String Owner = null;
+	public String Device = null;
 }
